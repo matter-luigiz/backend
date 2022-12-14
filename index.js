0d349fbf6875f816eb95b0d9c7b01e43d6a01fdb
Gabe Guralnick <gnguralnick@gmail.com>
2022-12-14 13:39:50 -0500

    fix: CORS issue in GET

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ app.get('/', (req, res) => {
 app.get('/:site', (req, res) => {
     const filePath = path.join(__dirname, 'sites', req.params['site'] + '.json');
     console.log(`[server]: Received request for site ${req.params['site']} -> path is ${filePath}`);
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials" : true
+    });
     if (fs.existsSync(filePath)) {
         console.log(`[server]: Found file for site ${req.params['site']}`);
-        res.sendFile(filePath);
+        res.status(200).sendFile(filePath);
     } else {
         res.status(404).send('Cannot find listing for site ' + req.params['site']);
     }
